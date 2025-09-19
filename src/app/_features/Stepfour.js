3f6ce67fdec2013eed509f61cef718fa4b315b25
25LP0902 25LP0902 <25LP0902@admins-iMac.local>
2025-09-19 12:27:13 +0800

    deleted '

--- src/app/_features/Stepfour.js
+++ src/app/_features/Stepfour.js
@@ -3,7 +3,7 @@ export const Stepfour = (props) => {
   return (
     <div className="container1">
       <div className="con1">
-        <img src="/Main 1.svg" /> <h1>You'&apos;re All Set 🔥</h1>
+        <img src="/Main 1.svg" /> <h1>You&apos;re All Set 🔥</h1>
         <p>We have received your submission. Thank you!</p>
         <button onClick={handleReDoStep} className="butt4">
           Change answer
